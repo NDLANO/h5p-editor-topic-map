49a609146412a4f64d4750741a1583a57919e17a
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2023-06-28 15:10:17 +0200

    feat: add hover effect on boxes and arrows
    and ensure unique arrow ids

--- src/H5P/H5P.util.ts
+++ src/H5P/H5P.util.ts
@@ -1,5 +1,8 @@
 import { H5PEditor } from 'h5p-utils';
 import { TranslationKey } from '../types/TranslationKey';
+import { H5PObject } from 'h5p-types';
+
+export const H5P = (window as any).H5P as H5PObject;
 
 export const t: (
   key: TranslationKey,
